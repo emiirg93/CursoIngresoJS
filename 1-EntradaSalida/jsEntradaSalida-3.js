7c658c221a275847e42da8048eb21fb8fb38d924
emiirg93 <emiliano.rg93@gmail.com>
2017-02-02 09:54:12 -0300

    resuelto
    con el profesor

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,12 +2,17 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	var m = document.getElementById("elNombre").value;
+	//creo una variable
+	var nombre;
+
+	nombre = document.getElementById('elNombre').value;
 	
-	alert (m);
-
-
-
+	/* document.g + tab + shift + i 
+	y coloco el ID del <imput ID="elNombre"> adentro del parenesis. 
+	por ultimo para que funcione en el final tengo que escribir .value
+	*/
+	
+	alert ("nombre ingresado es: " + nombre);
 }
 
 
