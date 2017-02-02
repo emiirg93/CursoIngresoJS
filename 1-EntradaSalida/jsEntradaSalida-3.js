fd8a7885b87ce32426c966d40d9a25580ca2c4ba
emiirg93 <emiliano.rg93@gmail.com>
2017-02-02 09:17:15 -0300

    ej 1 2 3
    resulto

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,8 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	var m = document.getElementById("elNombre").value ;
+	var m = document.getElementById("elNombre").value;
+	
 	alert (m);
 
 
