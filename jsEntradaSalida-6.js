03ff23f62646c9068e8ad0b17f60a57bbf6573c2
emiirg93 <emiliano.rg93@gmail.com>
2017-02-06 23:32:59 -0300

    resueltos
    je

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numuno;
+	var numdos;
+	var suma;
+
+	numuno=document.getElementById('numeroUno').value;
+	numdos=document.getElementById('numeroDos').value;
+
+
+	// alert ("la suma es "parseInt(numuno)+parseInt(numdos));
+
+
+	alert (parseInt(numuno)+parseInt(numdos));
+}
+
