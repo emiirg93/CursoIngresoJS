483fe0bdcdb30e9ae863a41ea7c4a6f641c0f11a
emiirg93 <emiliano.rg93@gmail.com>
2017-02-16 12:10:56 -0300

    16/02

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,37 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var numero;
+	var acumuladornegativo;
+	var acumuladorpositivo;
+	var respuesta=true;
 
+	while (respuesta == true)
+	{
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		numero = prompt ("Ingrese Numero.");
+		numero = parseInt (numero);
+		
+		if (numero<0)
+		{
+
+  			acumuladornegativo = negativo*numero;
+		
+		}
+
+		else
+		{
+
+			acumuladorpositivo = positivo+numero;
+
+		}
+		
+		contador = contador+1;
+
+		respuesta = confirm("¿Quiere ingresar otro numero?");
+	}
+
+document.getElementById('suma').value=acumuladorpositivo;
+document.getElementById('producto').value=acumuladornegativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,14 +3,49 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var max;
+	var min;
+	var numero;
 	var respuesta='si';
 
-	while(respuesta!='no')
-	{
+		while(respuesta!='no')
+		{
+			
+			numero = prompt ("Ingrese Numero.");
+			numero = parseInt(numero);
+			
+			if (contador == 0)
+			{
+
+				min=numero;
+				max=numero;
+
+			}
 		
-	
-	}
+			else
+			{
+
+				if (numero>max)
+				{
+
+					max = numero;
+
+				}
+				if (numero<min)
+				{
+
+					min = numero;
+				}
+
+			}
+			
+			contador = contador+1
+			respuesta = prompt ("¿Quiere ingresar otro numero?")
+
+		}
+
+	document.getElementById('maximo').value=numero;
+	document.getElementById('minimo').value=numero;
 
 
 
